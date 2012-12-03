676df326e92bdfb2109438703b90ff95ac0ab468
William Cobb <hello@bigwilliam.com>
2012-12-02 22:27:27 -0700

    added a few functions to play.py

--- PLAY.py
+++ PLAY.py
@@ -1,3 +1,18 @@
+# Steps of how the game will work
+'''
+1. The game starts, the user sees an intro screen (hits enter)
+2. The board is created and boats positions randomly generated
+3. asks for user name
+4. Screen displays board and asks for input
+5. input is checked for validity 
+6. compare input with boat values
+7. if found, update boat and board to 'x', run the "hit" screen for the user to see
+8. if miss, update board to 'o', run the 'miss' screen
+9. ask for next input
+'''
+
+
+
 # for starters, I'm going to create a board to see what it's supposed to look like
 def showBoard():
 	''' 
@@ -20,7 +35,7 @@ def showBoard():
 
 # Next, I want to create the board as a matrix so that it can be manipulated in the game
 
-def createBoard():
+def createBoardArray():
 	'''
 	Create the board using an array matrix
 	'''
@@ -38,23 +53,25 @@ def createBoard():
 	#print it so we can test it
 	#for row in board:
 	#	print(row)
-createBoard()
+#createBoardArray()
 
 
 # Another idea for displaying the board is to exclude the row/col headers completely from the board itself and only insert them when the board is being displayed. This way I can keep the board arrays pure without worring about header values (which should not be edited).
-# Also, when displaying the board, maybe I won't keep the spaces in the array values either, and just insert those when displaying it
-
-###
+# Also, when displaying the board, maybe I won't keep the spaces in the array values either, and just insert those when displaying itu
 
 # An idea for controlling the board in the background is to use a dictionary to store the values. This might be a good idea actually, because I can use keys to reference and update the board if it's a hit or miss or "."
-boardAsDictionary = { (A, 0):".", (A, 1):".", (A, 2):".", (A, 3):"." } # and so forth
 
-def createDictBoard():
+def createBoard():
 	'''
-	going to create the board as a dictionary
+	going to create the board as a dictionary where the keys are tuples
 	'''
-	
+	rowHeader = "0123456789"
+	colHeader = "ABCDEFGHIJ"
+	global board = {}
 
+	for i in colHeader:
+		for j in rowHeader:
+			board[(i,j)] = "."
 
 
 
