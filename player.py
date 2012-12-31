899a9dd9fa4fbcba2f02fb7d151a92380d8fbc0b
William Cobb <hello@bigwilliam.com>
2012-12-30 23:08:11 -0700

    added some methods to player.py

--- player.py
+++ player.py
@@ -1,19 +1,26 @@
 class Player:
-    """ Creates a Player object. Good for future implementations when this game will become multiplayer"""
+    """ Creates a Player object. Good for future implementations when this game will become multiplayer. """
 
     def __init__(self, iName):
         self.name = iName
         self.score = 0
+        self.numberOfMoves = 0
     
-    def getName(self):
+    # get functions
+
+    def get_Name(self):
         return self.name
 
-    def getScore(self):
-        # will return the player's score (future use)
-        return True
+    def get_Score(self):
+        return self.score
 
-    def updateScore(self, points):
-        return self.score + points
+    def get_numberOfMoves(self):
+        return self.numberOfMoves
 
+    # update functions
 
-        
+    def update_Score(self, points):
+        self.score = self.score + points
+
+    def update_numberOfMoves(self, moves = 1):
+        self.numberOfMoves += moves
