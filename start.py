ee5d52d7530e42869a41f4e015e753f35b89a864
William Cobb <hello@bigwilliam.com>
2013-01-19 18:45:24 -0700

    clarified comments in start.py

--- start.py
+++ start.py
@@ -13,6 +13,6 @@ raw_input( "Press Enter to continue..." )
 player = player.Player() # create the player object for 1 player (options: playerName = '')
 board = gameboard.GameBoard() # create the gameboard object (options: boardSize=10, setMark='.')
 
-newGame = game.Game(player, board) # create and run the game 
+newGame = game.Game(player, board) # create a game object with player and board
 
 newGame.runGame() # run the game object
