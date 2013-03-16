1d808668fbebd8c24682b89b1fe43172dc77348b
William Cobb <hello@bigwilliam.com>
2013-03-15 22:50:05 -0600

    created new function to validate moves

--- game.py
+++ game.py
@@ -39,6 +39,30 @@ class Game:
                     return boat
             return False
 
+        def moveIsValid(playerMove):
+            """ (str) -> bool
+
+            Validate the move. Return True if valid, False if not. 
+            Things to validate for:
+            1. 2 characters in length only
+            2. first is letter a-j, second is number 0-9
+            3. move has not been played already
+            THOUGHT: I'm thinking that the function getPlayerMove should handle this instead. aka combine this function with getPlayerMove.
+            """
+
+            # set up regex match
+            validMove = re.compile("[a-j][0-9]") # Missing: Still need to check for length!
+            if not validMove.match(playerMove):
+                return False
+
+            xMove = int(playerMove[1])
+            yMove = int( self.coordinateReference[playerMove[0].upper()] )
+
+            # check to be sure this move hasn't already been played, ask again if need be
+            if self.board.hasAlreadyBeenPlayed(xMove,yMove):
+                return False
+            
+            return True                
 
         # NOW WE'RE ACTUALLY PLAYING
         self.board.draw()
@@ -46,21 +70,20 @@ class Game:
             
             newMove = getPlayerMove()
 
+            ### CHECK IF MOVE IS VALID BEFORE CONTINUING
+            while not moveIsValid(newMove):
+
+                self.player.addToNumberOfMoves(moves = -1) # don't count the move because it is invalid
+
+                print("Ye scallywag! Yer move ain't right! Did you already play that? Also, yer move must look like this: A5, got it?")
+
+                newMove = getPlayerMove()
+
             # remember the user inputs coordinates as yx, but we need it to be xy, so here we do the switch
             xMove = int(newMove[1]) # [1] is the 2nd element of newMove
             yMove = int( self.coordinateReference[newMove[0].upper()] ) # [0] is the first element of newMove. convert letters to corresponding numbers we can use to update things with coordinates as int 
 
-            # set up regex match
-            validMove = re.compile("[a-j][0-9]")
-
-            # check to be sure this move hasn't already been played, ask again if need be
-            while self.board.hasAlreadyBeenPlayed(xMove,yMove) or len(newMove) > 2 or not validMove.match(newMove):
-                self.player.addToNumberOfMoves(moves = -1) # don't count the move because it was already played
-                print("Ye scallywag! Ye have already made that move! Try another!")
-                newMove = getPlayerMove()
-                # I have to repeat this process, which is why it might be placed inside of getPlayerMove()
-                xMove = int(newMove[1])
-                yMove = int( self.coordinateReference[newMove[0].upper()] )
+            
 
             # check to see if the move hits a boat   
             boatHit = moveHitsABoat(xMove, yMove, self.boatList) 
@@ -68,18 +91,15 @@ class Game:
             if boatHit: # the move was a HIT
                 boatHit.updateHit(xMove,yMove) # tell the boat to update coordinates with a hitMark
                 self.board.updateHit(xMove,yMove) # tell the board to update coordinates with a hitMark
-                #print("xxxxxxxxxxxxxxxxxxxxxx HIT xxxxxxxxxxxxxxxxxxxxxxxxxxxx") # tell the user it was a HIT
                 displayArt.Hit()
 
                 if boatHit.isSunk():
                     self.numberOfBoatsLeft = self.numberOfBoatsLeft - 1 # here's what the while loop is keeping track of
-                    #print(" ^^^^^^^^^^^^ Ye SUNK a ship!!! ^^^^^^^^^^^^^^")
                     displayArt.Sunk()
                     raw_input( "Press Enter to continue..." )
 
             else: # The move was a MISS
                 self.board.updateMiss(xMove,yMove)
-                #print("ooooooooooooooooooooo MISS ooooooooooooooooooooooooooooo")
                 displayArt.Miss()
 
             print()
